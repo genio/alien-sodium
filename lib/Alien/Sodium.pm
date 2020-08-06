93788a1649bc97541f01e2bd653cadba2c783c7b
Chase Whitener <cwhitener@gmail.com>
2020-08-05 22:09:00 -0400

    Modernize the build a bit: (#1)
    Modernize the build a bit:
    
    * Update the dzil and build in general
    * Use an alienfile
    * Use cpanfile for prereqs
    * Use DZil ::Starter
    * Keep makefile and meta in repo for testing
    * Keep license file in repo
    * Fixup the changes
    * Bump version to 2
    * Fix version specific docs
    * Switch to README.md
    * CI via GH Workflows/Actions
    * Add FFI test
    * Add XS test
    * Don't package upstream libsodium library with the build
    * Ensure we're always getting the latest stable version
    * Don't do PodWeaver for such a small doc

--- lib/Alien/Sodium.pm
+++ lib/Alien/Sodium.pm
@@ -1,33 +1,68 @@
-# ABSTRACT: Install and make available libsodium library
+package Alien::Sodium;
 
 use strict;
 use warnings;
-package Alien::Sodium;
-
+use utf8;
 use parent 'Alien::Base';
+
+our $VERSION = '2.000';
 
 1;
 
 __END__
 
+=encoding utf8
+
+=head1 NAME
+
+Alien::Sodium - Interface to the libsodium library L<http://libsodium.org>
+
 =head1 SYNOPSIS
 
-    use Alien::Sodium;
+    use strict;
+    use warnings;
 
-    my $cflags = Alien::Sodium->cflags;
-    my $libs = Alien::Sodium->libs;
-    
+    use ExtUtils::MakeMaker;
+    use Config;
+    use Alien::Base::Wrapper ();
+
+    WriteMakefile(
+      Alien::Base::Wrapper->new('Alien::Sodium')->mm_args2(
+        ...
+        CONFIGURE_REQUIRES => {
+          'Alien::Sodium' => '2.000',
+        },
+        ...
+      ),
+    );
+
 =head1 DESCRIPTION
 
-Alien::Sodium installs the C library C<libsodium> version 1.0.8 released on
-December 25, 2015.
+This package can be used by other L<CPAN|https://metacpan.org> modules that
+require L<libsodium|http://libsodium.org>.
 
 =head1 SEE ALSO
 
 =over 4
 
-=item * L<libsodium|https://github.com/jedisct1/libsodium>
+=item * L<libsodium|http://libsodium.org>
 
 =back
+
+=head1 AUTHOR
+
+Alex J. G. Burzyński <F<ajgb@cpan.org>>
+
+=head1 CONTRIBUTORS
+
+Graham Ollis <F<plicease@cpan.org>>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright (c) 2015 Alex J. G. Burzyński. All rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
 
 =cut
